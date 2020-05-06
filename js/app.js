6c3d51a0b2cf0d99251cde14a33f5c1a94c66e9d
armel <armelzong@gmail.com>
2020-05-06 15:48:02 +0000

    finishing_todo

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-var app= new Vue({
+var app = new Vue({
     el: '#app',
     data: {
-      actions:["Prier","Nettoyer","Balayer","Preparer"],
-      act:"",
-      pos:null
+        actions: ["Prier", "Nettoyer", "Balayer", "Preparer"],
+        act: "",
+        pos: null
     },
     methods: {
-       addaction(){
-           this.actions.push(this.act);
-       },
-       remove_action(){
-           this.actions.splice(this.pos-1,1);
-       }
+        addaction() {
+            this.actions.push(this.act);
+        },
+        remove_action() {
+            this.actions.splice(this.pos - 1, 1);
+        }
     },
     created() {
         console.log("L'affaire est en creation");
